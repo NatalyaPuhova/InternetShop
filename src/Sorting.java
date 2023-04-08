ee557f37a1a70a0bfcf0242314b13afdabd1e2c5
Pusa <mi_13@mail.ru>
2023-04-08 19:25:33 +0300

    first commit

---
+++ src/Sorting.java
@@ -0,0 +1,17 @@
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Sorting {
+
+
+
+
+    private void outPutProgramme(){
+        System.exit(0);
+    }
+
+}
+
